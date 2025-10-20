1199b1b0d824d5ce701b1672fd347d5c2944d20e
Catherine Hummel <chummel@franciscan.edu>
2025-10-20 10:42:46 -0400

    Added Carousel

--- assets/script.js
+++ assets/script.js
@@ -118,3 +118,334 @@ for (var i=0; i<franTabBtns.length; i++) {
         franTabBtns[i].onclick = updateSchedulePanelOnClick(i);
     }
 }
+
+
+
+
+
+
+
+// === Carousel ===================== //
+
+const allFranCarouselSections = document.querySelectorAll('.fran-carousel-section');
+
+if (allFranCarouselSections.length != 0) {
+
+    const allFranCarouselsSurrounds = document.querySelectorAll('.fran-carousel-surround');
+
+    const allFranCarousels = document.querySelectorAll('.fran-carousel');
+    const allFranCarouselCars = document.querySelectorAll('.fran-carousel-car');
+    const allFranCarouselLeftArrows = document.querySelectorAll('.fran-carousel-arrow-row .arrow-left');
+    const allFranCarouselRightArrows = document.querySelectorAll('.fran-carousel-arrow-row .arrow-right');
+    var allFranCarouselBtns = document.querySelectorAll('.fran-carousel-btn-row button');
+
+    var initCarouselLoad = false;
+    var moveButtonsActive = true;
+
+    var autoplayCarouselList = [];
+    var carouselOffsets = [];
+
+    const regMoveTimeout = 5;
+    const btnMoveTimeout = 10;
+
+
+    function setUpCarousels() {
+
+        // resize carousels to proper height
+        for (let i=0; i<allFranCarousels.length; i++) {
+            
+            var currentCars = allFranCarousels[i].querySelectorAll('.fran-carousel-car');
+            var largestCarHeight = 0;
+            
+            for (let j=0; j<currentCars.length; j++) {
+                if ( currentCars[j].clientHeight > largestCarHeight ) {
+                    largestCarHeight = currentCars[j].clientHeight;
+                }
+            }
+            allFranCarousels[i].style.height = largestCarHeight + 20 + 'px';
+
+            carouselOffsets.push(currentCars[0]);
+
+        }
+
+
+        // only perform on first load
+
+        if (!initCarouselLoad) {
+
+            initCarouselLoad = true;
+
+            for (let i=0; i<allFranCarouselSections.length; i++) {
+
+                // set correct amount of buttons for each carousel 
+                var carCount = (allFranCarouselSections[i].querySelectorAll('.fran-carousel-car').length - 1);
+            
+                var currentCarouselBtnRow = allFranCarouselSections[i].querySelector('.fran-carousel-btn-row');
+                var currentCarouselBtn = allFranCarouselSections[i].querySelector('.active-button');
+
+                for (let j=0; j<carCount; j++) {
+
+                    var newNode = currentCarouselBtn.cloneNode(true);
+                    newNode.classList.remove('active-button');
+                    currentCarouselBtnRow.appendChild(newNode);
+
+                }
+
+
+                // Get all autoplaying carousels
+                var currentCarousel = allFranCarouselSections[i].querySelector('.fran-carousel-surround');
+                if ( currentCarousel.dataset.autoplay == "true" ) {
+                    var singleCarouselProperties = [currentCarousel];
+
+                    var singleCarouselTimer = (currentCarousel.dataset.autoplayTimer != undefined) ? currentCarousel.dataset.autoplayTimer : regMoveTimeout;
+                    var singleCarouselBtnTimeout = (currentCarousel.dataset.autoplayBtnTimeout != undefined) ? currentCarousel.dataset.autoplayBtnTimeout : btnMoveTimeout;
+
+                    // set initial countdown
+                    currentCarousel.setAttribute("data-autoplay-countdown", singleCarouselTimer);
+
+                    singleCarouselProperties.push(singleCarouselTimer);
+                    singleCarouselProperties.push(singleCarouselBtnTimeout);
+
+                    autoplayCarouselList.push(singleCarouselProperties);
+                }
+
+            }
+        }
+        // reset buttons variable
+        allFranCarouselBtns = document.querySelectorAll('.fran-carousel-btn-row button')
+
+
+    }
+    setUpCarousels();
+
+
+    function carouselMove(num, index, direction='') {
+
+        if (moveButtonsActive) {
+
+            moveButtonsActive = false;
+
+            // carousel variables
+            var currentSection = allFranCarouselSections[index];
+            var currentSurround = allFranCarouselsSurrounds[index];
+            var currentCarousel = currentSection.querySelector('.fran-carousel');
+            var currentCarouselCars = currentCarousel.querySelectorAll('.fran-carousel-car');
+            var activeCar = currentCarousel.querySelector('.fran-carousel-car.active-car');
+            var nextCar = currentCarouselCars[num];
+
+            var activeCarIndex = Array.prototype.indexOf.call(currentCarouselCars, activeCar);
+
+
+            // reset countdown
+            for (let i=0; i<autoplayCarouselList.length; i++) {
+                if (autoplayCarouselList[i][0] == currentSurround) {
+                    currentSurround.setAttribute("data-autoplay-countdown", autoplayCarouselList[i][2]);
+                }
+            }
+
+            // Check that there is more than one car and that the requested index
+            // to move to is not the same as what is currently active
+            if ( currentCarouselCars.length > 1 && num != activeCarIndex ) {
+
+                // move right
+                if (direction == 'right') {
+
+                    directionRight();
+                    updateBtns(num, index);
+
+                }
+
+                else if (direction == 'left') {
+                    
+                    directionLeft();
+                    updateBtns(num, index);
+
+                }
+                else {
+                    
+                    if (num < activeCarIndex) {
+
+                        directionLeft();
+                        updateBtns(num, index);
+
+                    } else {
+
+                        directionRight();
+                        updateBtns(num, index);
+
+                    }
+                }
+
+            }
+
+            function directionRight() {
+                activeCar.classList.remove('active-car');
+                nextCar.classList.add('active-car');
+
+                activeCar.classList.add('car-moving-right-to-inactive');
+                nextCar.classList.add('car-moving-right-to-active');
+                
+            }
+            function directionLeft() {
+                activeCar.classList.remove('active-car');
+                nextCar.classList.add('active-car');
+
+                activeCar.classList.add('car-moving-left-to-inactive');
+                nextCar.classList.add('car-moving-left-to-active');
+            }
+
+
+            setTimeout(function () {
+                moveButtonsActive = true;
+
+                for (let i=0; i<currentCarouselCars.length; i++) {
+                    currentCarouselCars[i].classList.remove('car-moving-left-to-inactive');
+                    currentCarouselCars[i].classList.remove('car-moving-left-to-active');
+                    currentCarouselCars[i].classList.remove('car-moving-right-to-inactive');
+                    currentCarouselCars[i].classList.remove('car-moving-right-to-active');
+                }
+            
+            }, 500);
+
+        }
+
+    }
+
+
+    // update active button
+    function updateBtns(num, index) {
+
+        var currentButtonRow = allFranCarouselSections[index].querySelector('.fran-carousel-btn-row');
+        var activeBtn = currentButtonRow.querySelector('.active-button');
+        var requestedBtnIndex = Array.prototype.indexOf.call( currentButtonRow.querySelectorAll('button'), currentButtonRow.querySelectorAll('button')[num] );
+
+        if (requestedBtnIndex != -1) {
+            activeBtn.classList.remove('active-button');
+            currentButtonRow.querySelectorAll('button')[requestedBtnIndex].classList.add('active-button');
+        }
+
+    }
+
+
+
+
+    // move carousel car left
+    var moveCarouselLeft = function(index) {
+        return function() {
+
+            moveCarByOne(index, 'left');
+
+        }
+    }
+
+    // move carousel car right
+    var moveCarouselRight = function(index) {
+        return function() {
+
+            moveCarByOne(index, 'right');
+
+        }
+    }
+
+    var moveCarouselOnBtn = function(num) {
+        return function() {
+
+            //const allFranCarouselSections = document.querySelectorAll('.fran-carousel-section');
+            var currentCarouselSection = allFranCarouselBtns[num].closest('.fran-carousel-section');
+            var carouselIndex = Array.prototype.indexOf.call(allFranCarouselSections, currentCarouselSection);
+
+            if (carouselIndex != -1) {
+                carouselMove(num, carouselIndex);
+            }
+
+        }
+    }
+
+
+    // move carousel car left on click
+    for (var i=0; i<allFranCarouselLeftArrows.length; i++) {
+        if (Array.prototype.indexOf.call(allFranCarouselLeftArrows, allFranCarouselLeftArrows[i] == i)) {
+            allFranCarouselLeftArrows[i].onclick = moveCarouselLeft(i);
+        }
+    }
+
+    // move carousel car right on click
+    for (var i=0; i<allFranCarouselRightArrows.length; i++) {
+        if (Array.prototype.indexOf.call(allFranCarouselRightArrows, allFranCarouselRightArrows[i] == i)) {
+            allFranCarouselRightArrows[i].onclick = moveCarouselRight(i);
+        }
+    }
+
+        // move carousel on button click
+        for (var i=0; i<allFranCarouselBtns.length; i++) {
+        if (Array.prototype.indexOf.call(allFranCarouselBtns, allFranCarouselBtns[i] == i)) {
+            allFranCarouselBtns[i].onclick = moveCarouselOnBtn(i);
+        }
+    }
+
+    window.addEventListener("resize", setUpCarousels);
+
+
+    // move by one car, either left or right
+    function moveCarByOne(index, direction) {
+
+        var currentCarousel = document.querySelectorAll('.fran-carousel')[index];
+        var currentCarouselCarLen = currentCarousel.querySelectorAll('.fran-carousel-car').length;
+        var activeCarIndex = Array.prototype.indexOf.call( currentCarousel.querySelectorAll('.fran-carousel-car'), currentCarousel.querySelector('.fran-carousel-car.active-car') );
+        var moveToNum = 0;
+
+        if ( direction == 'right' ) {
+            if ( activeCarIndex + 1 >= currentCarouselCarLen ) {
+                moveToNum = 0;
+            } else {
+                moveToNum = activeCarIndex + 1;
+            }
+            
+            carouselMove(moveToNum, index, direction='right');
+        } else if ( direction == 'left' ) {
+            if ( activeCarIndex - 1 < 0 ) {
+                moveToNum = currentCarouselCarLen - 1;
+            } else {
+                moveToNum = activeCarIndex - 1;
+            }
+            
+            carouselMove(moveToNum, index, direction='left');
+        }
+
+    }
+
+
+    // autoplay carousels
+    function autoplayAllCarousels() {
+
+        for (let i=0; i<autoplayCarouselList.length; i++) {
+            
+            var currentList = autoplayCarouselList[i];
+            var currentCarousel = currentList[0];
+            var regTime = currentList[1];
+            var autoplayCountdown = currentCarousel.dataset.autoplayCountdown;
+
+            if (autoplayCountdown != undefined) {
+
+                autoplayCountdown -= 1;
+                if (autoplayCountdown <= 0) {
+
+                    var carouselIndex = Array.prototype.indexOf.call(allFranCarouselsSurrounds, currentCarousel);
+
+                    moveCarByOne(carouselIndex, 'right');
+                    currentCarousel.setAttribute("data-autoplay-countdown", regTime);
+
+                } else {
+
+                    currentCarousel.setAttribute("data-autoplay-countdown", autoplayCountdown);
+
+                }
+
+            }
+
+        }
+
+    }
+    setInterval(autoplayAllCarousels, 1000);
+
+}
